f483dfe1685b170a70e8a6115b06c76aea36aeea
amr639 <amr.639@gmail.com>
2020-07-14 20:01:12 +0530

    improving ux - changing color of input fields based on input item selection type for inc or exp

--- app.js
+++ app.js
@@ -183,6 +183,12 @@ var UIController = (function() {
         return (type === 'exp' ? '-' : '+') + ' ' + int + '.' + dec;
     };
 
+    var nodeListForEach = function(list, callback) {
+        for (var i =0; i < list.length; i++) {
+            callback(list[i], i);
+        }
+    };
+
     return {
         getinput: function() {
             return {
@@ -251,12 +257,6 @@ var UIController = (function() {
 
             var fields = document.querySelectorAll(DOMstrings.expensesPercLabel);
 
-            var nodeListForEach = function(list, callback) {
-                for (var i =0; i < list.length; i++) {
-                    callback(list[i], i);
-                }
-            };
-
             nodeListForEach(fields, function(current, index) {
                 if (percentages[index] > 0) {
                     current.textContent = percentages[index] + '%';
@@ -278,6 +278,22 @@ var UIController = (function() {
             year = now.getFullYear();
 
             document.querySelector(DOMstrings.dateLabel).textContent = months[month] + ' ' + year;
+        },
+
+        changedType: function() {
+
+            var fields = document.querySelectorAll(
+                DOMstrings.inputType + ',' +
+                DOMstrings.inputDescription + ',' +
+                DOMstrings.inputValue
+            );
+
+            nodeListForEach(fields, function(cur) {
+                cur.classList.toggle('red-focus');
+            });
+
+            document.querySelector(DOMstrings.inputBtn).classList.toggle('red');
+
         },
 
         getDomStrings: function() {
@@ -304,6 +320,8 @@ var controller = (function(budgetCtrl, UICtrl) {
         });
 
         document.querySelector(DOMlinks.container).addEventListener('click', ctrlDeleteItem);
+
+        document.querySelector(DOMlinks.inputType).addEventListener('change', UICtrl.changedType);
     };
 
     var updateBudget = function() {
